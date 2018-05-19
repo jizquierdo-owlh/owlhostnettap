197a611e53c80472fd558315c2b69bba74256e7b
jose antonio izquierdo <jizquierdo@osnids.com>
2018-05-19 18:39:09 +0200

    -- dummy interface config  --

--- dummy.sh
+++ dummy.sh
@@ -1,12 +1,12 @@
 sudo echo 'dummy' >> /etc/modules-load.d/dummy.conf
-sudo echo 'install dummy /sbin/modprobe --ignore-install dummy; /sbin/ip link set name ethdummy1 dev dummy0 ' >> /etc/modprobe.d/dummy.conf
+sudo echo 'install dummy /sbin/modprobe --ignore-install dummy; /sbin/ip link set name owlh dev dummy0 ' >> /etc/modprobe.d/dummy.conf
 
 sudo echo "
-NAME=ethdummy1
-DEVICE=ethdummy1
-MACADDR=00:22:22:ff:ff:ff
-IPADDR=10.10.10.1
-NETMASK=255.255.255.0
+NAME=owlh
+DEVICE=owlh
+#MACADDR=00:22:22:ff:ff:ff
+#IPADDR=10.10.10.1
+#NETMASK=255.255.255.0
 ONBOOT=yes
 TYPE=Ethernet
 NM_CONTROLLED=no
